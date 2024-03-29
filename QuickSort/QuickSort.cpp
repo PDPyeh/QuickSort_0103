0ccc78a5b793d5bbd387bdf53efa9bdb21af44f8
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-29 10:44:59 +0700

    cout untuk display

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -70,4 +70,8 @@ void q_short(int low, int high) {
 	q_short(low, j - 1);
 	q_short(j + 1, high);
 }
-void display()
+void display() {
+	cout << "\-----------" << endl;
+	cout << "Sorted array" << endl;
+	cout << "------------" << endl;
+}
