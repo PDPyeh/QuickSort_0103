942adbb6e9547d75ac16c23e42e77c29a2ae04af
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-29 10:45:39 +0700

    fungsi for dan cout untuk display

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -74,4 +74,11 @@ void display() {
 	cout << "\-----------" << endl;
 	cout << "Sorted array" << endl;
 	cout << "------------" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n\nNumber of comparisions: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+
 }
